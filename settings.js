eb1668ec6776d04af500f411b95e9be6bf65891f
udithsaranga12 <114066798+udithsaranga12@users.noreply.github.com>
2022-09-21 15:52:49 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,17 +36,17 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //OWNER CONTACT
-global.vcardowner = ['94711421243'] //ur owner number
-global.ownername = "ᴋɪɴɢ ᴅᴀʀᴋ ᴀʟᴘʜᴀ" //ur owner name
+global.vcardowner = ['94703277998'] //ur owner number
+global.ownername = " ۝⃟̥̩̩̩̥̩̥͚̮𝔘.𝔖۝⃟̥̩̩̩̥̩̥͚̮ " //ur owner name
 global.ytname = "YT: TGM Arcade" //ur yt chanel name
 global.socialm = "GitHub: darkalphaxteam" //ur github or insta name
 global.location = "Sri Lanka, Kurunagala, Nikaweratiya" //ur location
 
 //BOT COSTOMIZEDTATION
-global.owner = ['94711421243']
-global.ownertag = '94711421243' //ur tag number
-global.botnumber = '94764679736' //Add your Bot Number For Log
-global.botname = 'CYBER-X BETA EDITION' //ur bot name
+global.owner = ['94703277998']
+global.ownertag = '94703277998' //ur tag number
+global.botnumber = '94703277998' //Add your Bot Number For Log
+global.botname = '۝⃟̥̩̩̩̥̩̥͚̮𝔘.𝔖۝⃟̥̩̩̩̥̩̥͚̮-𝘈𝘘𝘜𝘈 𝘔𝘋 𝘉𝘖𝘛' //ur bot name
 global.ig = "https://youtu.be/uzMbW5v9QLE" //your theme url which will be displayed on whatsapp
 global.myweb = "https://youtu.be/uzMbW5v9QLE" //ur website to be displayed
 global.sc = 'https://github.com/darkalphaxteam' //script link
@@ -54,8 +54,8 @@ global.linkz = 'https://youtu.be/uzMbW5v9QLE'
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "📩" //ur theme emoji
 global.packname = "Cyber-X Test Bot" //ur sticker watermark packname
-global.author = "DARK ALPHA" //ur sticker watermark author
-global.footer = "® ᴄʏʙᴇʀ-x ʙᴇᴛᴀ ᴇᴅɪᴛɪᴏɴ"
+global.author = "۝⃟̥̩̩̩̥̩̥͚̮𝔘.𝔖۝⃟̥̩̩̩̥̩̥͚̮" //ur sticker watermark author
+global.footer = "۝⃟̥̩̩̩̥̩̥͚̮𝔘.𝔖۝⃟̥̩̩̩̥̩̥͚̮-𝘈𝘘𝘜𝘈 𝘔𝘋 𝘉𝘖𝘛"
 global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
 global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
 global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
@@ -70,7 +70,7 @@ global.thumb = fs.readFileSync("./LocalData/theme/cyber.jpg") //ur thumb pic
 
 //DATA BASED
 
-global.premium = ['94711421243'] //ADD YOU NUMBER
+global.premium = ['94703277998'] //ADD YOU NUMBER
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
